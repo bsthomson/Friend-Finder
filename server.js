a04a11272444b810d9fb8020991845f24f28733b
bsthomson <thomson.bs@gmail.com>
2018-08-05 15:14:08 -0500

    changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/", function(req, res) {
 
 app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "/app/public/survey.html"))
-})
+});
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
