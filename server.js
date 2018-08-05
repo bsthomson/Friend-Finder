2c80753014f2769c3f1b9ed7599285e6838b164d
bsthomson <thomson.bs@gmail.com>
2018-08-05 12:26:46 -0500

    changes

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/publice/reserve.html"))
+})
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
