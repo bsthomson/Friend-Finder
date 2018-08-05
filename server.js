26311fa0bab09cbd56a86daa148e6ea6821b8b3f
bsthomson <thomson.bs@gmail.com>
2018-08-05 11:42:43 -0500

    changes

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
