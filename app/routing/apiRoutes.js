70de96a7f9b3e9dee2b82ca78991be7f0be8c224
bsthomson <thomson.bs@gmail.com>
2018-08-11 17:57:40 -0500

    changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,39 @@
 const path = require('path');
 
+var friends = [
+    {
+        name: "Karl Marx",
+        picture: "http://www.historyguide.org/images/marx-bio.jpg",
+        score1: 5,
+        score2: 5,
+        score3: 5,
+        score4: 5,
+        score5: 5,
+        score6: 5,
+        score7: 5,
+        score8: 5,
+        score9: 5,
+        score10: 5
+    },
+    {
+        name: "Ayn Rand",
+        picture: "http://www.slate.com/content/dam/slate/archive/2009/11/1_123125_122946_2207169_2233141_091102_books_aynrand2tn.jpg.CROP.original-original.jpg",
+        score1: 1,
+        score2: 1,
+        score3: 1,
+        score4: 1,
+        score5: 1,
+        score6: 1,
+        score7: 1,
+        score8: 1,
+        score9: 1,
+        score10: 1
+    }
+];
+
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
+        console.log(friends);
         return res.json(friends);
     });
 
@@ -18,6 +50,17 @@ module.exports = function(app) {
         
         return res.json(false);
     });
+
+    app.post("/api/friends", function(req, res) {
+        
+        var newfriend = req.body;
+      
+        newfriend.name = newfriend.name.replace(/\s+/g, "").toLowerCase();
+      
+        friends.push(newfriend);
+      
+        res.json(newfriend);
+      });
 
     
 };
--- server.js
+++ server.js
@@ -7,37 +7,6 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var friends = [
-    {
-        name: "Karl Marx",
-        picture: "http://www.historyguide.org/images/marx-bio.jpg",
-        score1: 5,
-        score2: 5,
-        score3: 5,
-        score4: 5,
-        score5: 5,
-        score6: 5,
-        score7: 5,
-        score8: 5,
-        score9: 5,
-        score10: 5
-    },
-    {
-        name: "Ayn Rand",
-        picture: "http://www.slate.com/content/dam/slate/archive/2009/11/1_123125_122946_2207169_2233141_091102_books_aynrand2tn.jpg.CROP.original-original.jpg",
-        score1: 1,
-        score2: 1,
-        score3: 1,
-        score4: 1,
-        score5: 1,
-        score6: 1,
-        score7: 1,
-        score8: 1,
-        score9: 1,
-        score10: 1
-    }
-];
-
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
